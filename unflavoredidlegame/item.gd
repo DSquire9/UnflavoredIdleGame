bcd12575c75bda3ac5360aa1ed22b613f0c6be14
DSquire9 <dss9593@rit.edu>
2025-02-03 12:39:53 -0500

    Added timers to items

--- unflavoredidlegame/item.gd
+++ unflavoredidlegame/item.gd
@@ -5,10 +5,11 @@ class_name Item extends Node
 @export var timeToPayout = 0.0
 @export var amountHeld = 0
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Timer.wait_time = timeToPayout
+	if amountHeld > 0:
+		$Timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,4 +20,9 @@ func _send_payment() -> void:
 	get_parent().currency += (payout * amountHeld)
 	
 func buy(amount) -> void:
+	if amount == 0:
+		$Timer.start()
 	amountHeld += amount
+
+func _on_timer_timeout() -> void:
+	_send_payment()
